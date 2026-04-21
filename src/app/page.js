226ce93f17fa52ff3a5d6c41ad15ae53b5d8efbe
Francesco-Zucco <zuccoqwe@gmail.com>
2026-04-21 19:19:00 +0300

    first commit - created task manager logic

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,76 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [tasks, setTasks] = useState([{ text: "", done: false }]);
+  const [input, setInput] = useState("");
+
+  const addTask = () => {
+    if (!input) return;
+    setTasks([...tasks, { text: input, done: false }]);
+    setInput("");
+  };
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  const toggleDone = (index) => {
+    setTasks(
+      tasks.map((task, i) =>
+        i === index ? { ...task, done: !task.done } : task,
+      ),
+    );
+  };
+
+  useEffect(() => {
+    const saved = localStorage.getItem("tasks");
+    if (saved) {
+      setTasks(JSON.parse(saved));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="w-full flex items-center justify-center p-4">
+      <div className="max-w-[1024px] w-full">
+        <div className="w-full">
+          <nav className="w-full flex justify-between">
+            <h1>Task Manager</h1>
+            <div className="flex flex-row gap-2">
+              <input
+                className=" px-4 py-3 rounded-full border border-gray-300 focus:outline-none  focus:shadow-md transition shadow-sm
+  "
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+              />
+              <button
+                className="bg-[#cfff56] px-4 py-2 rounded-full"
+                onClick={addTask}
+              >
+                Add Task
+              </button>
+            </div>
+          </nav>
+
+          <ul>
+            {tasks.map((task, i) => (
+              <li
+                style={{
+                  color: task.done ? "green" : "black",
+                  textDecoration: task.done ? "line-through" : "none",
+                }}
+                key={i}
+              >
+                {task.text} <button onClick={() => toggleDone(i)}>✅</button>
+                <button onClick={() => deleteTask(i)}>x</button>
+              </li>
+            ))}
+          </ul>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
